5f7331c796fd5abfc887d280c741d99d9a7e085a
alilooo <ali@aliloo.com>
2025-05-15 23:37:30 +0200

    Fix dropdown options: Add SimpleActionOptionsProviderPlugin to correctly add discount option to Apply dropdown

---
+++ app/code/Stilecht/BuyXGetY/Plugin/SalesRule/Model/Rule/Action/SimpleActionOptionsProviderPlugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright © Stilecht BuyXGetY All rights reserved.
+ */
+
+namespace Stilecht\BuyXGetY\Plugin\SalesRule\Model\Rule\Action;
+
+class SimpleActionOptionsProviderPlugin
+{
+    /**
+     * Add custom "Buy X Get Y Cheapest Free" action to the available actions
+     *
+     * @param \Magento\SalesRule\Model\Rule\Action\SimpleActionOptionsProvider $subject
+     * @param array $result
+     * @return array
+     */
+    public function afterToOptionArray(
+        \Magento\SalesRule\Model\Rule\Action\SimpleActionOptionsProvider $subject,
+        $result
+    ) {
+        // Add our custom discount type
+        $result[] = [
+            'label' => __('Buy X Get Y Cheapest Free'),
+            'value' => 'buy_x_get_y_cheapest_free'
+        ];
+        
+        return $result;
+    }
+} 
